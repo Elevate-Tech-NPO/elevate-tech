d4066045944ce003da3366ce52bafa284d58440a
= <lilyxmeng13@gmail.com>
2022-08-01 18:40:53 -0400

    update

--- js/elevatehacks.js
+++ js/elevatehacks.js
@@ -4,7 +4,7 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-countdown = new Date("Aug 13, 2021 20:00:00").getTime();
+countdown = new Date("Aug 20, 2022 00:00:00").getTime();
 
 x = setInterval(function () {
   var now = new Date().getTime();
@@ -47,7 +47,7 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("schedule-slide");
   var dots = document.getElementsByClassName("schedule-dot");
-  var day1 = 13;
+  var day1 = 20;
 
   if (n > slides.length) {
     slideIndex = 1
@@ -64,32 +64,14 @@ function showSlides(n) {
   }
 
   var date = day1 + slideIndex - 1;
-  document.getElementById("schedule-date").innerHTML = "August " + date + "th";
+  document.getElementById("schedule-date").innerHTML = "August " + date;
 
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
 
 var events = [{ name: "Zoom Opening", description: "Zoom Opening" },
-{ name: "Opening Ceremony", description: "Opening Ceremony and Keynote", link: "https://www.youtube.com/watch?v=JQTtX4mIx9k&list=PLDFkmHUo2QidJrgeyIIYszy6GCqfIXKMA&index=1" },
-{ name: "Team Formation", description: "Team Formation via Zoom" },
-{ name: "Hackathons 101 by Akshaya Dinesh", description: "Learn to make the best of your hackathon experience from our opening ceremony keynote presenter, Stanford student and entrepreneur Akshaya Dinsesh!", link: "https://www.youtube.com/watch?v=JQTtX4mIx9k&list=PLDFkmHUo2QidJrgeyIIYszy6GCqfIXKMA&index=1" },
-{ name: "Creative Coding with Python - ImagiLabs", description: "Creative Coding with Python - ImagiLabs" },
-{ name: "Intro to GitHub", description: "Intro to GitHub with Gauri Nair. Learn how to operate Github and collaborate on your hackathon project!", link: "https://www.youtube.com/watch?v=3yMr_opRem4&list=PLDFkmHUo2QidJrgeyIIYszy6GCqfIXKMA&index=5" },
-{ name: "Intro to Web Development (Qoom)", description: "Intro to Web Development with Qoom with Alexandria Bandle. Learn how to build a website from scratch using Qoom's IDE", link: "https://www.youtube.com/watch?v=dc4TgfeGong&list=PLDFkmHUo2QidJrgeyIIYszy6GCqfIXKMA&index=2"},
-{ name: "AI Ethics with Ria Cherevu", description: "AI Ethics with Intel researcher Ria Cherevu", link: "https://www.youtube.com/watch?v=kNpWDAefUFQ&list=PLDFkmHUo2QidJrgeyIIYszy6GCqfIXKMA&index=3"},
-{ name: "Women in Tech Panel", description: "Women in Tech Panel: hear from and ask questions to women in the tech industry", link: "https://www.youtube.com/watch?v=bJDqJf64n6k&list=PLDFkmHUo2QidJrgeyIIYszy6GCqfIXKMA&index=7" },
-{ name: "College Tech Leaders Panel", description: "College Tech Leaders Panel: hear from and ask questions to women studying STEM in college!", link: "https://www.youtube.com/watch?v=yb_7or5j9To&list=PLDFkmHUo2QidJrgeyIIYszy6GCqfIXKMA&index=6" },
-{ name: "Intro to iOS Development", description: "Intro to iOS Development Mikaela Caron. Build your first iOS application using Swift and Xcode!" },
-{ name: "Intro to Machine Learning", description: "Intro to Machine Learning", link: "https://www.youtube.com/watch?v=-AsT2Qk2ROY&list=PLDFkmHUo2QidJrgeyIIYszy6GCqfIXKMA&index=8" },
-{ name: "Creative Coding with Python (ImagiLabs)", description: "Creative Coding with Python by ImagiMentor Andrea Maggino.", link: "https://www.youtube.com/watch?v=Vod1YQGTr5k&list=PLDFkmHUo2QidJrgeyIIYszy6GCqfIXKMA&index=4" },
-{ name: "Pitching Basics (and Hackathon Judging Debrief)", description: "Pitching Basics (and Hackathon Judging Debrief) with Director of Outreach, Aanvi Koolwal", link: "https://www.youtube.com/watch?v=jMH7sTvFu_s&list=PLDFkmHUo2QidJrgeyIIYszy6GCqfIXKMA&index=9" },
-{ name: "Zoom Reopens", description: "Zoom Reopens" },
-{ name: "Submissions Due", description: "Submissions Due" },
-{ name: "Submission Validation", description: "Submission Validation" },
-{ name: "Intro to LinkedIn &amp; Resume Building", description: "Intro to LinkedIn & Resume Building by influencer and computer science PhD student, Liz Victoria", link: "https://www.youtube.com/watch?v=igUV_POcdtI&list=PLDFkmHUo2QidJrgeyIIYszy6GCqfIXKMA&index=10"},
-{ name: "Social Hour &amp; Game Period", description: "Social Hour & Game Period" },
-{ name: "Awards and Closing Ceremony", description: "Awards and Closing Ceremony", link: "https://www.youtube.com/watch?v=gufU_iGD3p4&list=PLDFkmHUo2QidJrgeyIIYszy6GCqfIXKMA&index=11" }
+{ name: "Opening Ceremony", description: "Opening Ceremony and Keynote", link: "https://www.youtube.com/watch?v=JQTtX4mIx9k&list=PLDFkmHUo2QidJrgeyIIYszy6GCqfIXKMA&index=1" }
 ];
 
 function clickedScheduleRow(row) {
@@ -100,11 +82,6 @@ function clickedScheduleRow(row) {
     if (events[i].name === eventTitle) {
       document.getElementById("event-description-text-testing").innerHTML = events[i].description;
       document.getElementById("event-description-link").setAttribute("href", events[i].link);
-      if (events[i].link !== undefined) {
-        document.getElementById("event-description-link").innerHTML = "View the recording here";
-      } else {
-        document.getElementById("event-description-link").innerHTML = "";
-      }
     }
   }
 }
